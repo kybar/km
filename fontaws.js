a3e3906e1c92ad57172caaaef488302c31aaeb30
kybar <lucky.bachtiar@gmail.com>
2016-08-04 15:39:38 +0700

    Create fontaws.js

---
+++ fontaws.js
@@ -0,0 +1,3 @@
+//<![CDATA[
+function loadCSS(e,t,n){"use strict";var o=window.document.createElement("link"),s=t||window.document.getElementsByTagName("script")[0];o.rel="stylesheet",o.href=e,o.media="only x",s.parentNode.insertBefore(o,s),setTimeout(function(){o.media=n||"all"})}loadCSS("//maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css");
+//]]>
